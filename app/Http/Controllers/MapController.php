1d8a1d9bbdf9996533fb6a7d6258e6b58d50d7be
bhautik <rathodbhautikrameshbhai@gmaial.com>
2022-06-01 13:41:55 +0530

    add sql file

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\map;
+use Illuminate\Http\Request;
+
+class MapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function show(map $map)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(map $map)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, map $map)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(map $map)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/StoreaddressController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\storeaddress;
+use Illuminate\Http\Request;
+
+class StoreaddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\storeaddress  $storeaddress
+     * @return \Illuminate\Http\Response
+     */
+    public function show(storeaddress $storeaddress)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\storeaddress  $storeaddress
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(storeaddress $storeaddress)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\storeaddress  $storeaddress
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, storeaddress $storeaddress)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\storeaddress  $storeaddress
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(storeaddress $storeaddress)
+    {
+        //
+    }
+}
